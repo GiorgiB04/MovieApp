ded67d9c080c6f1b08445bb5f5363ac9b9d197d4
Giorgi Botchorishvili <giorgibochorishvili04@gmail.com>
2024-12-07 00:21:50 +0100

    redirect rating color

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,6 +1,8 @@
 export const ratingColor = (rating) => {
   if (rating >= 7) {
     return "bg-green-500 px-2 py-1 rounded-lg text-lg font-bold";
+  } else if (rating >= 6) {
+    return "bg-yellow-500 px-2 py-1 rounded-lg text-lg font-bold";
   } else if (rating >= 5) {
     return "bg-orange-600 px-2 py-1 rounded-lg text-lg font-bold";
   } else {
