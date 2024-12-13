b62417e540a2e063e4b1cd5287600e5563f166a3
Giorgi Botchorishvili <giorgibochorishvili04@gmail.com>
2024-12-13 23:31:57 +0100

    fix shows page: popular

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -46,7 +46,7 @@ const Shows = () => {
             >
               <option
                 className="border-slate-500 rounded-full"
-                value="popularity.desc&vote_count.gte=150"
+                value="popularity.desc&vote_count.gte=100&without_genres=10763%2C%2010764"
               >
                 Popular
               </option>
