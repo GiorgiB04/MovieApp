af9c0d67938effd24c73013704fec7f84dcacc5a
Giorgi Botchorishvili <giorgibochorishvili04@gmail.com>
2025-04-26 15:31:54 +0200

    menu colo changed: cyan-500

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             to={item.href}
             className="relative group text-sm font-semibold tracking-widest uppercase"
           >
-            <span className="text-white group-hover:text-blue-500 transition-colors duration-300">
+            <span className="text-white group-hover:text-cyan-500 transition-colors duration-300">
               {item.label}
             </span>
             <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-gradient-to-r from-sky-500 cyan-500 blue-500 group-hover:w-full transition-all duration-500 ease-out"></span>
